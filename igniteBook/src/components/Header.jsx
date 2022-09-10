02abe349fc1ce473cf69af950763ddb62fc68dbd
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-09-09 23:18:19 -0300

    header finalizado

---
+++ igniteBook/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import style from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
+
+export function Header() {
+    return(
+
+        <header className={style.header}>
+
+        <img src={igniteLogo} alt='logotipo ignite' />
+        </header>
+
+    )
+}
